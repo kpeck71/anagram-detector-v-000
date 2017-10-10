e35d3bff9de5bb623474c27b7755f7afda284a6d
Kristen Peck <kpeck71@gmail.com>
2017-10-10 11:47:36 +0000

    automatically backed up by learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -9,14 +9,14 @@ class Anagram
   end
 binding.pry
 
-  def match(anagrams)
+  def match(word,anagrams)
     #iterate over the array of words (anagrams) that match takes as an argument
     #compare each word of that array to the word that the Anagram class is initialized with
     #split each word of the potential anagram into an array of letters using some_word.split("")
     #compare two arrays with == and also .sort the arrays to see if they contain the same elements in the same order (and are therefore equal)
 
     @@anagrams.map do |w|
-      word = word.split("").sort
+      word2 = word.split("").sort
       w = w.split("").sort
       if w == word
         put word
