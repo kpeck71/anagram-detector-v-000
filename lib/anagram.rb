c56ceb18a74ea56a52c0b37a3c2c51432c1c2b59
Kristen Peck <kpeck71@gmail.com>
2017-10-10 11:49:46 +0000

    automatically backed up by learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -9,12 +9,12 @@ class Anagram
   end
 binding.pry
 
-  def match(word,anagrams)
-    #iterate over the array of words (anagrams) that match takes as an argument
-    #compare each word of that array to the word that the Anagram class is initialized with
-    #split each word of the potential anagram into an array of letters using some_word.split("")
-    #compare two arrays with == and also .sort the arrays to see if they contain the same elements in the same order (and are therefore equal)
+#iterate over the array of words (anagrams) that match takes as an argument
+#compare each word of that array to the word that the Anagram class is initialized with
+#split each word of the potential anagram into an array of letters using some_word.split("")
+#compare two arrays with == and also .sort the arrays to see if they contain the same elements in the same order (and are therefore equal)
 
+  def match(word,anagrams)
     anagrams.map do |w|
       word = word.split("").sort
       w = w.split("").sort
@@ -22,8 +22,6 @@ binding.pry
         put word
       end
     end
-
-
   end
 
 
