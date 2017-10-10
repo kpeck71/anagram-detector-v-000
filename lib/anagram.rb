ab6da14aa4a23e7597e256ee3d91da567ebe0ed5
Kristen Peck <kpeck71@gmail.com>
2017-10-10 12:11:24 +0000

    automatically backed up by learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -13,9 +13,6 @@ class Anagram
 #split each word of the potential anagram into an array of letters using some_word.split("")
 #compare two arrays with == and also .sort the arrays to see if they contain the same elements in the same order (and are therefore equal)
 
-
-binding.pry
-
   def match(anagrams)
     matches = []
     anagrams.each do |w|
