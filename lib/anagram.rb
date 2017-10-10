d2779abab6ec6fad2daf9f406b2f6a73a682feb5
Kristen Peck <kpeck71@gmail.com>
2017-10-10 12:13:27 +0000

    automatically backed up by learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -16,9 +16,8 @@ class Anagram
   def match(anagrams)
     matches = []
     anagrams.each do |w|
-      sorted = w.split("").sort
-      if word.split("").sort == sorted
-        matches << sorted.join("")
+      if word.split("").sort == w.split("").sort
+        matches << w
       end
     end
     matches
